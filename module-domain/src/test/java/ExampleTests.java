bba2a1326ae7f8e9208c9b0a5f83e709e180094e
Chia-Yang Sun <triumph.sun@gmail.com>
2021-10-06 19:55:09 +0800

    Introduce module-specific gradle.properties file.

---
+++ module-domain/src/test/java/ExampleTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTests {
+
+    @Test
+    public void exampleTest(){
+        Assertions.assertEquals(1, 1);
+    }
+}
